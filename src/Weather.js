fcefcbe4f02bfc1c03b1276d9fd576d3b71a8a73
Michelle Harley <harleyma1851@gmail.com>
2023-01-18 13:59:50 -0600

    install axios

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 export default function Weather() {
-  const apiKey = "ee92b3aa969be9b032535442tfo57250";
+  let apiKey = "ee92b3aa969be9b032535442tfo57250";
   let city = "Lisbon";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=ee92b3aa969be9b032535442tfo57250&units=metric`;
   return (
